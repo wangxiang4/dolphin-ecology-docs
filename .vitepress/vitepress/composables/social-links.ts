e807e9dd44ec76aa5de0f8bb6aed1d8b88b7fd0c
wangxiang <1827945911@qq.com>
2023-01-05 23:09:25 +0800

    🐬 海豚生态计划-重构

--- .vitepress/vitepress/composables/social-links.ts
+++ .vitepress/vitepress/composables/social-links.ts
@@ -3,7 +3,7 @@ import GitHubIcon from '~icons/ri/github-fill'
 export const useSocialLinks = () => {
   return [
     {
-      link: 'https://github.com/element-plus/element-plus',
+      link: 'https://github.com/wangxiang4/dolphin-ecology-docs',
       icon: GitHubIcon,
       text: 'GitHub',
     },
