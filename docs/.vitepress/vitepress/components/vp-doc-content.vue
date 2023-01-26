985ce62cdf23c34c708bc3a2d7efc8bbe0cc8298
wangxiang <1827945911@qq.com>
2023-01-27 03:40:19 +0800

    🐧 write docs

--- docs/.vitepress/vitepress/components/vp-doc-content.vue
+++ docs/.vitepress/vitepress/components/vp-doc-content.vue
@@ -1,28 +1,15 @@
 <script lang="ts" setup>
-import { ref } from 'vue'
 import { useData } from 'vitepress'
-import { insertLinkIcon, insertTableWrapper } from '../utils'
 import VPPageNav from './doc-content/vp-page-nav.vue'
 import VPTableOfContent from './doc-content/vp-table-of-content.vue'
 
 const { page } = useData()
-const content = ref<{ $el: HTMLElement }>()
-
-function updateDom() {
-  insertLinkIcon(content)
-  insertTableWrapper(content)
-}
 </script>
 
 <template>
   <div class="doc-content-wrapper">
     <div class="doc-content-container">
-      <Content
-        ref="content"
-        class="doc-content"
-        @vnode-mounted="updateDom"
-        @vnode-updated="updateDom"
-      />
+      <Content class="doc-content" />
       <VPPageNav />
     </div>
     <VPTableOfContent v-if="page.headers" />
