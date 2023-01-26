7fb93fb817343cc75e7b30c39ca84eabc1058e01
wangxiang <1827945911@qq.com>
2023-01-27 06:56:56 +0800

    🐧 write docs

---
+++ index.vue
@@ -0,0 +1,17 @@
+<template>
+<!--
+用作github Languages语言统计，
+github不会检测docs、examples目录下的文件，
+所以不会显示Languages模块
+-->
+</template>
+
+<script>
+export default {
+  name: 'index'
+}
+</script>
+
+<style scoped>
+
+</style>
